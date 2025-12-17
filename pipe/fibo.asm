1f7e9643fee1c495e6565dff462997301d61b673
ceciliasd <ceciliasd@estudante.ufscar.br>
2025-12-17 10:23:37 -0300

    Refactor Fibonacci

--- pipe/fibo.asm
+++ pipe/fibo.asm
@@ -1,30 +1,19 @@
-.text
+.text	# 0x00000000 rom
 .globl _start
 _start:
-    la   s0, fib_data        # ponteiro para RAM
-    li   s1, 0x00000104      # endereço MMIO dos LEDs
-
-    lw   t0, 0(s0)           # fib(0)
-    lw   t1, 4(s0)           # fib(1)
-
+	addi s0, zero, 4
+	addi s1, zero, 0x0000010C
+	lw t0, -4(s0)
+	lw t1, (s0)
 loop:
-    add  t2, t0, t1          # fib(n)
-    sw   t2, 8(s0)           # guarda na RAM
-    sw   t2, 0(s1)           # escreve nos LEDs
-    mv   t0, t1
-    mv   t1, t2
-    addi s0, s0, 4           # avança vetor
-
-    j loop                   # 7 instruções por iteração
-
-.data
-.align 2
-
-fib_data:
-    .word 0
-    .word 1
-    .space 64              # espaço para a sequência
-
-fib_out:
-    .word 0
-
+	add t2, t1, t0
+	add t0, zero, t1
+	add t1, zero, t2
+	addi s0, s0, 4
+	sw t2, (s0)
+	sw t2, (s1)
+	j loop
+	
+.data	# 0x00000000 ram
+a:	.word 0
+b:	.word 1
